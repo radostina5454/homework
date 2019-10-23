23b0726e6324f5bb74e944a4d0f1a22cb48d4f47
radostina5454 <56546651+radostina5454@users.noreply.github.com>
2019-10-23 11:00:13 +0300

    Create Radostina_python_task3.py

---
+++ Radostina_python_task3.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+def sum1(lst):
+    total = 0
+    for element in lst:
+        if type(element) is list:
+            total = total + sum1(element)
+        else:
+            total = total + element
+    return total
+
+
+if __name__ == '__main__':
+
+	t=[[1,2,7],4,3,5,[3,4]]
+	result = sum1(t)
+
+print(f' Sum is:{result}')
